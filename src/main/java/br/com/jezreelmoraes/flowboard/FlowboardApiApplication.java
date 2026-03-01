d1c453196c1a9518ff29696c7a36eae4335dc6f9
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2026-03-01 20:48:05 -0300

    feat: Application start

---
+++ src/main/java/br/com/jezreelmoraes/flowboard/FlowboardApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.jezreelmoraes.flowboard;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowboardApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowboardApiApplication.class, args);
+    }
+}
