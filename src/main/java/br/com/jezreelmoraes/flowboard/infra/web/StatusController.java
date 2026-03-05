f248a3a6cb89ae3b3259e42246c44aed982f36ef
Jezreel Kintino de Moraes <jezreel.moraes@asaas.com.br>
2026-03-04 22:25:15 -0300

    feat: Add CORS config to allow angular requests and add /ping endpoint

--- src/main/java/br/com/jezreelmoraes/flowboard/infra/web/StatusController.java
+++ src/main/java/br/com/jezreelmoraes/flowboard/infra/web/StatusController.java
@@ -1,19 +1,28 @@
 package br.com.jezreelmoraes.flowboard.infra.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/status")
+@RequestMapping("/api/v1")
 public class StatusController {
 
-    @GetMapping
+    @GetMapping("/status")
     public Map<String, Object> status() {
         return Map.of(
             "service", "FlowBoard API",
             "time", Instant.now()
         );
+    }
+
+    @GetMapping("/ping")
+    public ResponseEntity<Map<String, String>> ping() {
+        return ResponseEntity.ok(Map.of(
+            "message", "pong",
+            "status", "API UP"
+        ));
     }
 }
